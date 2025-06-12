7ba7adfa1410c5d7e949ddf883f710331ee85661
HAN <kennnshuit@gmail.com>
2025-06-12 15:45:40 +0900

    修正

--- src/Exception2.java
+++ src/Exception2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 
-public class Exception {
+public class Exception2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int result = 0;
